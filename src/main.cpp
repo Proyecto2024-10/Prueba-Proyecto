9932f9f57c746434c83f82e146b8dd863c256d62
Santiago del Sel <proyecto.delselgay@gmail.com>
2024-08-02 09:46:34 +0200

    modified:   platformio.ini 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,27 @@
-#include <WiFi.h>
 
 
-const char* ssid = "WIFI1";     //Nombre de tu wifi 
-const char* password = "del.sel1"; //Contraseña de tu wifi
+// Incluímos la librería para poder controlar el servo
+#include <ESP32Servo.h>
 
-WiFiServer server(80);  
-
-const int ledPin = 2;  
-
-void sendHttpResponse(WiFiClient client, String content) {
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-type:text/html");
-  client.println();
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>" + content + "</html>");
-  client.println();
-}
-
+ 
+Servo servo; 
+ 
+int pos = 0;   
+ 
 void setup() {
-  Serial.begin(115200); 
-
- 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);  
-
- 
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Conectando a WiFi...");
-  }
-
-  Serial.print("Conectado a WiFi. Dirección IP: ");
-  Serial.println(WiFi.localIP());
-
-  server.begin();  
-  Serial.println("Servidor iniciado");
+   // put your setup code here, to run once:
+   servo.attach(23); 
 }
-
-void loop() {
-  WiFiClient client = server.available();  
-
-  if (client) {
-    Serial.println("Nuevo cliente conectado");
-
-    String request = "";
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        request += c;
-
-        if (c == '\n') {
-          Serial.println("Petición completa recibida:");
-          Serial.println(request);
-
  
-          int commandIndex = request.indexOf("/?command=");
-          if (commandIndex != -1) {
-            String command = request.substring(commandIndex + 10, request.indexOf(" ", commandIndex + 10));
-            command.trim(); 
+void loop() {
+   // put your main code here, to run repeatedly:
+   for (pos = 0; pos <= 180; pos += 1){ 
+      servo.write(pos);              
+      delay(10);                       
+   }
 
-            
-            if (command.equalsIgnoreCase("on")) {
-              digitalWrite(ledPin, HIGH); 
-              Serial.println("LED Encendido");
-              sendHttpResponse(client, "<h1>LED Encendido</h1>");
-            } else if (command.equalsIgnoreCase("off")) {
-              digitalWrite(ledPin, LOW);
-              Serial.println("LED Apagado");
-              sendHttpResponse(client, "<h1>LED Apagado</h1>");
-            } else {
-              Serial.println("Comando desconocido");
-              sendHttpResponse(client, "<h1>Error: Comando desconocido</h1>");
-            }
-          } else {
-            Serial.println("Comando no encontrado en la solicitud");
-            sendHttpResponse(client, "<h1>Error: Comando no encontrado</h1>");
-          }
-          break;  
-        }
-      }
-    }
-    client.stop();
-    Serial.println("Cliente desconectado");
-  }
+   for (pos = 180; pos >= 0; pos -= 1){ 
+      servo.write(pos);              
+      delay(10);                       
+   }
 }

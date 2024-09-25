27a8c3133ceb4098c225ab52c2f38f74cb3af1cf
Santiago del Sel <proyecto.delselgay@gmail.com>
2024-09-25 10:38:45 +0200

    new file:   .vscode/settings.json 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,20 @@
-#include <AccelStepper.h>
+#include <Arduino.h>
+ 
+  #define STEP 33
+  #define DIR 32
 
-//Pines puestos de ejemplo (no son los que se van a utilizar)
-const int dirPin1 = 15;  // Motor 1 DIR
-const int stepPin1 = 76; // Motor 1 STEP
-const int dirPin2 = 34;  // Motor 2 DIR
-const int stepPin2 = 23; // Motor 2 STEP
-const int dirPin3 = 17;  // Motor 3 DIR
-const int stepPin3 = 18; // Motor 3 STEP
+  void setup()
+  {
+    pinMode(STEP, OUTPUT);
+    pinMode(DIR, OUTPUT);
+  }
 
-AccelStepper motor1(1, stepPin1, dirPin1);
-AccelStepper motor2(1, stepPin2, dirPin2);
-AccelStepper motor3(1, stepPin3, dirPin3);
+  void loop() {
+    digitalWrite (DIR, HIGH);
 
-void setup() {
+      digitalWrite(STEP, HIGH);
+      delayMicroseconds(500);
+      digitalWrite(STEP, LOW);
+      delayMicroseconds(500);
 
-    motor1.setMaxSpeed(700);      
-    motor1.setAcceleration(300);  
-    motor1.moveTo(600);   
-    motor2.setMaxSpeed(700);     
-    motor2.setAcceleration(300); 
-    motor2.moveTo(600);         
-
-    motor3.setMaxSpeed(700);     
-    motor3.setAcceleration(300);
-    motor3.moveTo(600);           
-}
-
-void loop() {
-   
-    motor1.run();
-    motor2.run();
-    motor3.run();
-}
+  }

23388186b24e2996af5c213731d785f90e82611f
Santiago del Sel <proyecto.delselgay@gmail.com>
2024-10-04 09:37:06 +0200

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,44 @@
+#include <Arduino.h>
 
+#define pinServo 0
 
-// Incluímos la librería para poder controlar el servo
-#include <ESP32Servo.h>
+unsigned long tiempoAnterior = 0;
+unsigned long duracionPulso = 0;
+unsigned long inicioPulso = 0;
+bool enviandoPulso = false;
 
- 
-Servo servo; 
- 
-int pos = 0;   
- 
+int angulos[] = {55, 45, 65, 45};  // Patrones de ángulos
+int indiceAngulo = 0;
+unsigned long intervaloCambio = 500; // Cambia de ángulo cada 500 ms
+
 void setup() {
-   // put your setup code here, to run once:
-   servo.attach(23); 
+  pinMode(pinServo, OUTPUT);
 }
- 
-void loop() {
-   // put your main code here, to run repeatedly:
-   for (pos = 0; pos <= 180; pos += 1){ 
-      servo.write(pos);              
-      delay(10);                       
-   }
 
-   for (pos = 180; pos >= 0; pos -= 1){ 
-      servo.write(pos);              
-      delay(10);                       
-   }
+void loop() {
+  unsigned long tiempoActual = millis();  
+
+  // Si no se está enviando un pulso, iniciamos un nuevo pulso
+  if (!enviandoPulso) {
+    
+    duracionPulso = map(angulos[indiceAngulo], 0, 180, 600, 2400); 
+    inicioPulso = micros();  
+    digitalWrite(pinServo, HIGH);
+    enviandoPulso = true;
+  }
+
+  // Si se ha alcanzado la duración del pulso, apagamos el pin
+  if (enviandoPulso && (micros() - inicioPulso >= duracionPulso)) {
+    digitalWrite(pinServo, LOW);
+    enviandoPulso = false;
+  }
+
+  // Cambiar ángulo cada 500 milisegundos
+  if (tiempoActual - tiempoAnterior >= intervaloCambio) {
+    tiempoAnterior = tiempoActual;
+    indiceAngulo++;
+    if (indiceAngulo >= 4) {
+      indiceAngulo = 0; // Reiniciar el índice después del último ángulo
+    }
+  }
 }

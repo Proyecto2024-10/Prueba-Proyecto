41ff7c9e9af79651bc7097efa8ca870abe935546
Santiago del Sel <proyecto.delselgay@gmail.com>
2024-10-25 07:54:47 +0200

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+
 #include <Arduino.h>
 
 #define pinServo 0
@@ -7,7 +8,7 @@ unsigned long duracionPulso = 0;
 unsigned long inicioPulso = 0;
 bool enviandoPulso = false;
 
-int angulos[] = {55, 45, 35, 45};  // Patrones de ángulos
+int angulos[] = {102, 100, 98, 100};  // Patrones de ángulos
 int indiceAngulo = 0;
 unsigned long intervaloCambio = 500; // Cambia de ángulo cada 500 ms
 
